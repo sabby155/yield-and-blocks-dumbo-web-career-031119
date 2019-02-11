2d7b0370f21419cd04a9e8c6bc7a0d35d94ec3b7
Sabrina Chowdhury <nebula155@gmail.com>
2019-02-11 16:54:14 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,5 @@
 def hello_t (array)
+  if block_given?
   i = 0
   
   while i < array.length 
@@ -6,6 +7,7 @@ def hello_t (array)
   i = i + 1
   end
   array
+else puts 
 end
 
 # call your method here!
